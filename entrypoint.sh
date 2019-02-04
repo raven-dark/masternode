629dd72fd350bf0d13e8c745a2e6899abeb8f2e9
Jayson <j4ys0n@gmail.com>
2019-02-03 17:02:50 -0700

    run cron, make sure rsyslog is running

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./ravendarkd -daemon -datadir=/root/data -conf=/root/conf/ravendark.conf
-
-touch /root/data/debug.log
-
+./ravendarkd -daemon -datadir=/root/data -conf=/root/conf/ravendark.conf && \
+touch /root/data/debug.log && \
+cron && \
+service rsyslog restart && \
 tail -n 100 -f /root/data/debug.log
